c86363505bf2786436b2775f0f4f225878022e0c
emersonrodrigov <emerson.rodrigo@gmail.com>
2018-09-26 08:12:39 -0300

    first commit

---
+++ target/classes/SparkRead.scala
@@ -0,0 +1,24 @@
+
+
+object SparkRead {
+  
+  
+  
+  
+  def main(args: Array[String]): Unit = {
+    
+    // LOGS 
+    import org.apache.log4j.{Level, Logger}
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+    
+    
+    
+    
+    
+    
+     
+  }
+  
+  
+}
